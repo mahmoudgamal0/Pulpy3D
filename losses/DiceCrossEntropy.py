17c17ffffee990d430a6416660fda05cc223a6b3
Knights-AI <mahmoud.gamal.15497@gmail.com>
2024-07-02 07:12:26 +0300

    Semantic Segmentation Test

--- losses/DiceCrossEntropy.py
+++ losses/DiceCrossEntropy.py
@@ -2,12 +2,10 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def dice_loss(pred, target, num_classes, smooth=1e-5):
-    # target_one_hot and pred are both [B C P P P]
-    target_one_hot = F.one_hot(target, num_classes=num_classes).permute(0, 4, 1, 2, 3).float()
-
-    intersection = torch.sum(pred * target_one_hot, dim=(0, 2, 3, 4))
-    cardinality = torch.sum(pred + target_one_hot, dim=(0, 2, 3, 4))
+def dice_loss(pred, target, smooth=1e-5):
+    # target and pred are both [B C P P P]
+    intersection = torch.sum(pred * target, dim=(0, 2, 3, 4))
+    cardinality = torch.sum(pred + target, dim=(0, 2, 3, 4))
 
     dice_scores = (2.0 * intersection + smooth) / (cardinality + smooth)
     average_dice = torch.mean(dice_scores)
@@ -16,11 +14,10 @@ def dice_loss(pred, target, num_classes, smooth=1e-5):
 
 
 def softmax_dice_loss(pred, target, num_classes, ce_weight=0.5, dice_weight=0.5, partition_weights=None):
-    # Target is GT
-    # Pred are Probabilities and not Logits
     target = target.squeeze(1).long()
-    ce_loss = nn.CrossEntropyLoss(weight=partition_weights)(pred, target)
-    dice_loss_val = dice_loss(pred, target, num_classes)
+    target_one_hot = F.one_hot(target, num_classes=num_classes).permute(0, 4, 1, 2, 3).float()
+    ce_loss = nn.CrossEntropyLoss(weight=partition_weights)(pred, target_one_hot)
+    dice_loss_val = dice_loss(pred, target_one_hot)
     loss = ce_weight * ce_loss + dice_weight * dice_loss_val
     return loss
 
